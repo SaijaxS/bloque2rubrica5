9c303eee1527ee66dd3ed17140418fc3820fd18d
SaijaxS <94907212+SaijaxS@users.noreply.github.com>
2022-12-16 11:56:17 +0100

    realizacion proyecto equipos nba

---
+++ bloue2rubrica5Javierburillo/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "baloncesto",
+
+    'summary': """baloncesto""",
+    
+    'description': """
+    un equipo de baloncesto
+    """,
+
+    'author': "javier burillo",
+    'website': "rfebm.com",
+    'category': 'b2r5',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views.xml',
+    ],
+}
